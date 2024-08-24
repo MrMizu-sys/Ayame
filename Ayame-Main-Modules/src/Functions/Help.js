8f7582c2ecf3ffa9ca0f6292b589a0a64419f4a6
Mizuyoshi <muzan4980@gmail.com>
2024-08-24 11:36:11 +0300

    Update Help.js

--- Ayame-Main-Modules/src/Functions/Help.js
+++ Ayame-Main-Modules/src/Functions/Help.js
@@ -14,10 +14,6 @@ function HelpFunction(msg, args, notice, moment, Prefix, Discord, AyameClient, D
    helpEmbed.addField(`**Moderation**`, `${notice}\`Ban, Kick.\``)
    helpEmbed.addField(`**Welcomer**`, `${notice}\`DisableWelcomerChannel, DisableWelcomerDM, DisableWelcomerImage, EnableWelcomerDM, SetWelcomerChannel, SetWelcomerImage, SetWelcomerMessage, SetWelcomerMessageDM, TestWelcomer, TestWelcomerDM.\``)
    helpEmbed.addField(`**Config**`, `${notice}\`DisableCommandsChannel, DisableNsfwCommands, EnableCommandsChannel, EnableNsfwCommands.\``)
-   if(!msg.channel.nsfw) {
-   helpEmbed.addField(`**Nsfw**`, `${notice}\`These Commands can only be seen in NSFW Channels!\``)
-   } else {
-     helpEmbed.addField(`**Nsfw**`, `${notice}\`Anal, Classic, NsfwAvatar, Tits.\``)
    }
    helpEmbed.addField(`**Links**`, `${notice}[Invite Link](https://discord.com/api/oauth2/authorize?client_id=745140408694079559&permissions=2134900151&scope=bot), [Github](https://github.com/zedxos/Ayame).`)
    return msg.channel.send(helpEmbed)
